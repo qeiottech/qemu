699e417a70e49cc360fa36b4de0747e190d1d323
Liu Jialu <liu.jialu@eco-ev.com>
2025-11-20 01:53:39 +0800

    seabios uefi

--- seabios/getall.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# URL: https://www.seabios.org/Download.html
-git clone https://git.seabios.org/seabios.git
---
+++ seabios/src/getall.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# URL: https://www.seabios.org/Download.html
+git clone https://git.seabios.org/seabios.git
