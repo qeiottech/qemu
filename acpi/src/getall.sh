deebe3a7be1f36220113964f4d595a60f74007b8
Liu Jialu <liu.jialu@eco-ev.com>
2026-03-06 02:12:01 +0800

    acpi

---
+++ acpi/src/getall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# URL: http://acpica.org
+# URL: https://www.intel.com/content/www/us/en/developer/topic-technology/open/acpica/download.html
+git clone git://github.com/acpica/acpica.git
