1ad9d4d10a376ba5377d4dc10867e7efcc5b7d1e
Liu Jialu <liu.jialu@eco-ev.com>
2026-08-21 17:53:53 +0800

    pxe install slackware64-current

--- slackware64-current/mkall.sh
+++ slackware64-current/mkall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # URL: https://qemu.weilnetz.de/doc/qemu-doc.html
-qemu-img create slackware64-current.img 50G
+../qemu/build/qemu-img create slackware64-current.img 50G
 
 # for install with ISO
 # qemu-system-x86_64 -m 2048 -hda slackware64-current.img -cdrom /home/jaloo/src/slackware64-current.iso -boot once=d
@@ -10,3 +10,20 @@ qemu-system-x86_64 -m 2048 -drive file=slackware64-current.img,index=0,media=dis
 # qemu-system-x86_64 -m 2048 -hda slackware64-current.img 
 qemu-system-x86_64 -m 2048 -drive file=slackware64-current.img,index=0,media=disk,format=raw
 
+# PXE
+# 1. tftpserver
+# URL: https://karellen.blogspot.com/2014/04/setting-up-tftp-server-with-slackware.html
+# vi /etc/inetd.conf
+# sh /etc/rc.d/rc.inetd restart
+
+# 2. PXE
+# URL: /tftpboot/slackware64-current/pxelinux.cfg/default
+# add console=ttyS0,115200 >> generic.s's append
+
+# 3. NFS
+# URL: https://wiki.qemu.org/Documentation/Networking
+# server ip address: 10.0.2.2
+# nfs path: /nfs_share/slackware64-current/slackware64/
+
+# URL: slackware64-current/usb-and-pxe-installers/README_PXE.TXT
+../qemu/build/qemu-system-x86_64 -enable-kvm -cpu host -m 2048 -drive file=./slackware64-current.img,index=0,media=disk,format=raw -nic user,tftp=/tftpboot/slackware64-current,bootfile=/pxelinux.0 -nographic
